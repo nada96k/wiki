976f95e4e39060862090f04053fe4bb77a20e92b
Your Namenada96k <nadawe_96@hotmail.com>
2019-10-02 21:09:02 +0300

    mini project is done

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+
+
+	def __str__(self):
+		return self.title
+
+	
+	def get_absolute_url(self):
+		return '/detail/%s'%(self.id)
+		# return reverse("page-detail", kwargs={"page_id": self.id})
+		# import the reverse
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+def Page_list(request):
+	context = {
+
+		"pages":Page.objects.all(),	
+	}
+	return render(request, 'list.html', context)
+
+def Page_detail(request,page_id):
+	context = {
+		"page":Page.objects.get(id=page_id),	
+	}
+	return render(request, 'detail.html', context)
